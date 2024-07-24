2c01958388a0c45e09528ee27972e369763562ea
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-07-24 14:53:11 -0600

    Prioridad de los operadores aritmeticos

--- Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
@@ -10,19 +10,12 @@ namespace Mi_Primer_Proyecto
     {
         static void Main(string[] args)
         {
-            Boolean comprobar = true;
-            Console.WriteLine("El valor es: {0}", comprobar);
+            //Prioridad de los operadores aritmeticos.
+            //se pone el sufijo m para la division con decimales.
+            decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1;
+            Console.WriteLine("La respuesta es: {0}", respuesta);
 
-            Console.Write("Nombre: ");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("Hola {0} bienvenido", nombre);
-
-            DateTime fechaActual = new DateTime();
-            fechaActual = DateTime.Now;
-            Console.WriteLine("La fecha actual es: {0}", fechaActual);
-
-            //Hace una pausa en la consola lo cual nos permite leer lo que se imprime
-            Console.ReadLine();
+            Console.WriteLine("La division de 5/4 es {0}", 5 / 4);
         }
     }
 }
