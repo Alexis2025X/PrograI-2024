0feb20e8113ec81c2f91b1aaaf08f44223f3e22d
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-07-30 09:59:04 -0600

    Uso del switch y llamadas a metrodos, saltos de lineas

--- Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
@@ -10,35 +10,109 @@ namespace Mi_Primer_Proyecto
     {
         static void Main(string[] args)
         {
-            //Estructuras de control
-            //1.If. Ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
+            //estructuras de control.
+            //2. Switch ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
             string continuar = "s";
-            while (continuar == "s") { 
+            while (continuar == "s")
+            {
+                Console.WriteLine("*** MENU ***");
+                Console.WriteLine("1. Promedio Notas");
+                Console.WriteLine("2. Promedio Serie Numeros");
+                Console.WriteLine("3. Clasificacion edad");
+                Console.WriteLine("4. Salir");
+                Console.Write("Opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1://if(opcion==1)
+                        promedio();
+                        break;
+                    case 2: //if(opcion==2)
+                        promedioSerie();
+                        break;
+                    case 3://if(opcion==3)
+                        clasificacionEdad();
+                        break;
+                    case 4://if(opcion==4)
+                        continuar = "n";
+                        break;
+                    default://else
+                        Console.WriteLine("opcion incorrecta \n\n");
+                        break;
+                }
+
+            }
+        }
+        static void promedio()
+        {
+            Console.Write("Lab1: ");
+            double lab1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Lab2: ");
+            double lab2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Parcial 1: ");
+            double parcial1 = double.Parse(Console.ReadLine());
+
+            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C1 es: {0}", c1);
+
+            Console.Write("Lab1: ");
+            lab1 = double.Parse(Console.ReadLine()); 
+
+            Console.Write("Lab2: ");
+            lab2 = double.Parse(Console.ReadLine()); 
+
+            Console.Write("Parcial 1: ");
+            parcial1 = double.Parse(Console.ReadLine()); 
+
+            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C2 es: {0}", c2);
+        }
+        static void promedioSerie()
+        {
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; 
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma += num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+        }
+        static void clasificacionEdad()
+        {
             Console.Write("Edad: ");
             int edad = int.Parse(Console.ReadLine());
 
             if (edad < 0)
             {
-                Console.WriteLine("Edada incorrecta");
-            }else if (edad <= 2){
-                Console.WriteLine("Eres un bebe");
-            }else if (edad < 12) {
-                Console.WriteLine("Eres un niño");
-            }else if (edad < 18) {
-                Console.WriteLine("Eres un adolescente");
-            }else if (edad < 65) {
-                Console.WriteLine("Bienvenido al mundo de las responsabilidades");
-            }else if (edad < 80) {
-                Console.WriteLine("Eres un adulto mayor");
-            }else {
-                 Console.WriteLine("Larga vida");
+                Console.WriteLine("Edad incorrecta.");
             }
-
-            Console.Write("Desea continuar? si desea continuar presione s sino quiere continuar presione cualquir tecla. \n\n ");
-                //Continuar
-                continuar = Console.ReadLine();
-             }
-
+            else if (edad <= 2)
+            {
+                Console.WriteLine("Eres un bebe");
+            }
+            else if (edad < 12)
+            {
+                Console.WriteLine("Eres un niño");
+            }
+            else if (edad < 18)
+            {
+                Console.WriteLine("Eres un adolescente.");
+            }
+            else if (edad <= 65)
+            {
+                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+            }
+            else if (edad <= 80)
+            {
+                Console.WriteLine("Eres un adulto mayor");
+            }
+            else
+            {
+                Console.WriteLine("Larga vida");
             }
         }
+    }
 }
