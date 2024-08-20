5f3eb1b07f7aa66e8b78ad0b51d75bf1701584bb
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-08-20 07:13:10 -0600

    Uso de funciones

--- Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
@@ -9,25 +9,17 @@ namespace Mi_Primer_Proyecto
     class Program
     {
         static void Main(string[] args)
-        { //determinar si cada numero de la serie es primo
-            int[] serie = new int[] { 5, 9, 4, 6, 3, 2, 7, 11, 15, 17, 21};
-            foreach (int num in serie)
-            { int i = 1,
-                n = 0;
-                // Y, And = &&
-                while(i<=num && n <= 2)
-                {
-                    if(num % i == 0)
-                    {
-                        n++;
-                    }
-                    i++;
-                }
-                Console.WriteLine("El numero {0} es {1}", num, n <= 2 ? "Primo" : "No es primo");
-                
-            }
+        {
+
+            //Uso de funciones
+            Console.WriteLine("La suma de 10+5 = {0}", suma());
             Console.ReadLine();
 
+        }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
         }
     }
 }
