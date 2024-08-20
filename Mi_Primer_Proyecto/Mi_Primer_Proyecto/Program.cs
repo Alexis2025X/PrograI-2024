faf78a77be7f91f79e7958de2e9e537fbaa54f80
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-08-20 07:21:31 -0600

    Uso de funciones con parametros

--- Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
@@ -12,13 +12,28 @@ namespace Mi_Primer_Proyecto
         {
 
             //Uso de funciones
+            //Suma sin parametros
             Console.WriteLine("La suma de 10+5 = {0}", suma());
+            
             Console.ReadLine();
 
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1} = {2}", num1, num2, suma2(num1, num2));
         }
         static int suma()
         {
             int respuesta = 10 + 5;
+            return respuesta;
+        }
+        //Suma con parametros
+        static int suma2 (int a, int b)
+        {
+            int respuesta = a + b;
             return respuesta;
         }
     }
