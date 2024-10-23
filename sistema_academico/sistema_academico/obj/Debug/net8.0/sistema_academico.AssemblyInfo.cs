6b29cb55aaca38afdde6344d81c9897280b844db
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-10-23 17:28:02 -0600

    Terminacion del CRUD alumnos

--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c8f3959c6643c68762d8a5b3c3a8af74b9139b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc60d82d3efde2bd956c9992b311f7c990307542")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
