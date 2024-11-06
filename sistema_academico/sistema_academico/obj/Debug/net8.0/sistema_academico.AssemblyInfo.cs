33bee325b879b0cfba06d4aa9dcef83d2f2f871c
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-11-06 10:31:33 -0600

    creacion del formulario docentes

--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68b6df9d50087e40512028e706a15db3ca5737e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f4fcae2c5bb7d2997d9f21df0d45d427d5ae58")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
