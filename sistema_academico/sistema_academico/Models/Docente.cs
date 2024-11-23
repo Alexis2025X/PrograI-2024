d13a64c2e71d06868d8b459aad7c3218298f259c
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-11-23 07:56:30 -0600

    Creacion de formularios (matricula) avanzados maestro detalle

---
+++ sistema_academico/sistema_academico/Models/Docente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace sistema_academico.Models
+
+{
+    public class Docente
+    {
+        [Key]
+        public int idDocente { get; set; }
+        public String codigo { get; set; }
+        public String nombre { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+        //byte intermedio entre numeros y letras 
+        public byte[] foto { get; set; }
+    }
+}
