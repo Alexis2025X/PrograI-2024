c07036228cdf4a0be6d6eba3cf906bf8cc6b462d
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-12-01 08:39:40 -0600

    PROYECTO FINAL TERMINADO

--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08fb07812e561096d88e151dfe552882dc86801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082dbf2cced9f8ad4b004c2bf68a86030096e0b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
